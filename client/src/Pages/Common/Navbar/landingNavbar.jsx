6527f12854d298fbfaf20385532cce2b9271fb70
Anand R P <anandrp.sics@gmail.com>
2024-07-20 16:48:21 +0530

    user register

---
+++ client/src/Pages/Common/Navbar/landingNavbar.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutSuccess } from "../../../redux/slices/authSlice";
+import useLocalStorage from "../../../customHooks/useLocalStorage";
+
+export const LandingNavbar = () => {
+  const { setDataToRedux } = useLocalStorage();
+
+  useEffect(() => {
+    setDataToRedux();
+  }, []);
+  const navigate = useNavigate();
+  const redirectUserLogin = () => {
+    navigate("/user-login");
+  };
+  const redirectFreelancerLogin = () => {
+    navigate("/freelancer-login");
+  };
+  const redirectFreelancer = () => {
+    navigate("/freelancer");
+  };
+  const redirectHome = () => {
+    navigate("/");
+  };
+
+  const viewAllConsultancies = () => {
+    navigate("/user-view-consultancies");
+  };
+
+  const redirectConsultancyLogin = () => {
+    navigate("/consultancy-login");
+  };
+
+
+  return (
+    <div className="container-fluid bg-connect " id="freelance-common-navbar">
+      <div className="connect justify-content-center ">
+        <span>FreelanceFlow</span>
+        <i className="bi bi-gear-wide-connected mx-3"></i>
+      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav1"
+            aria-controls="navbarNav1"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNav1"
+          >
+            <ul className="navbar-nav">
+              <li
+                className="nav-item m-3"
+                style={{ cursor: "pointer" }}
+                onClick={redirectHome}
+              >
+                <p className="nav-link">Home</p>
+              </li>
+              <li
+                className="nav-item m-3"
+                style={{ cursor: "pointer" }}
+                onClick={redirectFreelancer}
+              >
+                <p className="nav-link">Freelancers</p>
+              </li>
+              <li
+                className="nav-item m-3"
+                style={{ cursor: "pointer" }}
+                onClick={viewAllConsultancies}
+              >
+                <p className="nav-link">Consultancies</p>
+              </li>
+
+              <li className="nav-item dropdown m-3">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Login
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li onClick={redirectUserLogin} style={{ cursor: "pointer" }}>
+                    <a className="dropdown-item">User</a>
+                  </li>
+                  <li
+                    onClick={redirectFreelancerLogin}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <p className="m-0 dropdown-item">Freelancer</p>
+                  </li>
+                  <li
+                    onClick={redirectConsultancyLogin}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <p className="dropdown-item">Consultancy</p>
+                  </li>
+                  {/* <li>
+                    <a className="dropdown-item" href="#">
+                      Admin
+                    </a>
+                  </li> */}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
--- client/src/Pages/User/register/user_register.js
+++ client/src/Pages/User/register/user_register.js
@@ -4,7 +4,10 @@ import { useState } from "react";
 import { isEmailValid } from "../../../utils/validations/emailValidation";
 import { axiosInstance } from "../../../apis/axiosInstance";
 import { useNavigate } from "react-router-dom";
-import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
+import { LandingNavbar } from "../../Common/Navbar/landingNavbar";
+import Footer from "../../Common/Footer/footer";
+import LoginImg1 from "../../../Assets/new/login-img-1.png";
 function User_register() {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({
@@ -23,7 +26,7 @@ function User_register() {
 
   const navigateUserLogin = () => {
     navigate("/user-login");
-  }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     const { firstName, lastName, email, password } = userData;
@@ -48,8 +51,8 @@ function User_register() {
       let res = await axiosInstance.post("/userRegistration", userData);
       if (res.status === 201) {
         toast.success("Registration Successfull");
-        
-          redirectToUserLogin();
+
+        redirectToUserLogin();
       }
     } catch (error) {
       let responseStatus = error.response?.status || null;
@@ -70,124 +73,139 @@ function User_register() {
     }
   };
   return (
-    <div className="user-register container">
-      <div className="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
-        <div className="d-flex align-items-center justify-content-center w-100">
-          <div className="row justify-content-center w-100">
-            <div className="col-4">
-              <div className="card mb-0 p-4">
-                <div className="card-body">
-                  <h2 className="text-center user-register-heading">
-                    User Registration
-                  </h2>
-                  <form onSubmit={handleSubmit} className="needs-validation">
-                    <div className="mb-3">
-                      <label
-                        htmlFor="firstName"
-                        className="form-label user-login-label"
-                      >
-                        First Name
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="firstName"
-                        onChange={handleChanges}
-                        value={userData.firstName}
-                        name="firstName"
-                        required
-                      />
+    <>
+      <LandingNavbar />
+      <div className="user-register container">
+        <div className="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+          <div className="mt-5">
+            <div>
+              <h2 className="text-center  user-register-heading">
+                User Registration
+              </h2>
+            </div>
+            <div className="row justify-content-between gap-4 w-100  ">
+              <div className="col-5 ">
+                <img
+                  src={LoginImg1}
+                  className="w-100 h-100"
+                  alt="user_register"
+                />
+              </div>
+              <div className="col-5 ">
+                <div className="card mb-0 p-4">
+                  <div className="card-body">
+                    <form onSubmit={handleSubmit} className="needs-validation">
+                      <div className="mb-3">
+                        <label
+                          htmlFor="firstName"
+                          className="form-label user-login-label"
+                        >
+                          First Name
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="firstName"
+                          onChange={handleChanges}
+                          value={userData.firstName}
+                          name="firstName"
+                          required
+                        />
 
-                      <div class="invalid-feedback">
-                        Please provide your first Name.
+                        <div class="invalid-feedback">
+                          Please provide your first Name.
+                        </div>
                       </div>
-                    </div>
-                    <div className="mb-3">
-                      <label
-                        htmlFor="lastName"
-                        className="form-label user-login-label"
-                      >
-                        Last Name
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="lastName"
-                        required
-                        onChange={handleChanges}
-                        name="lastName"
-                        value={userData.lastName}
-                      />
-                    </div>
-                    <div className="mb-3 ">
-                      <label
-                        htmlFor="email"
-                        for="validationCustomUsername"
-                        className="form-label user-login-label"
-                      >
-                        Email address
-                      </label>
-
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        onChange={handleChanges}
-                        value={userData.email}
-                        name="email"
-                        aria-describedby="emailHelp"
-                        required
-                      />
-
-                      <div id="emailHelp" className="form-text">
-                        We'll never share your email with anyone else.
+                      <div className="mb-3">
+                        <label
+                          htmlFor="lastName"
+                          className="form-label user-login-label"
+                        >
+                          Last Name
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="lastName"
+                          required
+                          onChange={handleChanges}
+                          name="lastName"
+                          value={userData.lastName}
+                        />
                       </div>
-                    </div>
-                    <div className="mb-3">
-                      <label
-                        htmlFor="password"
-                        className="form-label user-login-label"
-                      >
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        minLength="6"
-                        onChange={handleChanges}
-                        name="password"
-                        value={userData.password}
-                        required
-                      />
-                    </div>
+                      <div className="mb-3 ">
+                        <label
+                          htmlFor="email"
+                          for="validationCustomUsername"
+                          className="form-label user-login-label"
+                        >
+                          Email address
+                        </label>
 
-                    <button
-                      type="submit"
-                      className="btn w-100 py-8 fs-4 mb-4 rounded-2 user-login-button text-white"
-                    >
-                      Register
-                    </button>
-                  </form>
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                  <p className="fs-5 mb-0 fw-bold">
-                    Already have an account?
-                    <span
-                      style={{ cursor: "pointer" }}
-                      onClick={navigateUserLogin}
-                      className="fw-bold ms-2 text-decoration-none text-primary"
-                    >
-                      Login
-                    </span>
-                  </p>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          onChange={handleChanges}
+                          value={userData.email}
+                          name="email"
+                          aria-describedby="emailHelp"
+                          required
+                        />
+
+                        <div id="emailHelp" className="form-text">
+                          We'll never share your email with anyone else.
+                        </div>
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="password"
+                          className="form-label user-login-label"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          minLength="6"
+                          onChange={handleChanges}
+                          name="password"
+                          value={userData.password}
+                          required
+                        />
+                      </div>
+
+                      <button
+                        type="submit"
+                        className="btn w-100 py-8 fs-4 mb-4 rounded-2 user-login-button text-white"
+                      >
+                        Register
+                      </button>
+                    </form>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-center">
+                    <p className="fs-5 mb-0 fw-bold">
+                      Already have an account?
+                      <span
+                        style={{ cursor: "pointer" }}
+                        onClick={navigateUserLogin}
+                        className="fw-bold ms-2 text-decoration-none text-primary"
+                      >
+                        Login
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="mt-5">
+        <Footer />
+      </div>
+    </>
   );
 }
 
