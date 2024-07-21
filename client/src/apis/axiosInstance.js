4ead9fc58276ecfe868a55356e644af2706429cf
Anand R P <anandrp.sics@gmail.com>
2024-07-22 00:39:09 +0530

    new build

--- client/src/apis/axiosInstance.js
+++ client/src/apis/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "http://hybrid.srishticampus.in/freelancers_marketplace_api/",
-    baseURL: "http://localhost:4036/freelancers_marketplace_api/",
+  baseURL: "http://hybrid.srishticampus.in/freelancers_marketplace_api/",
+    // baseURL: "http://localhost:4036/freelancers_marketplace_api/",
     headers: {
       "Content-Type": "application/json",
     },
