f6d3b03993f87130b50eaf2d18556a6936e6eec2
Anand R P <anandrp.sics@gmail.com>
2024-08-21 20:09:11 +0530

    new build

--- client/src/apis/axiosMultipart.js
+++ client/src/apis/axiosMultipart.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const axiosMultipartInstance = axios.create({
-  // baseURL: "http://hybrid.srishticampus.in/freelancers_marketplace_api/",
-  baseURL:  "http://localhost:4036/freelancers_marketplace_api/",
+  baseURL: "http://hybrid.srishticampus.in/freelancers_marketplace_api/",
+  // baseURL:  "http://localhost:4036/freelancers_marketplace_api/",
 
   headers: {
     "Content-Type": "multipart/form-data",
